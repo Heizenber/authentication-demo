43c2f0b9bf22ade8d6cdecd7f38864f82034ad2d
Heizenberg <popov.roman91@gmail.com>
2024-01-10 10:31:49 +0200

    added signup and form verification for stronger password

--- routes/demo.js
+++ routes/demo.js
@@ -1,29 +1,69 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const db = require('../data/database');
 
+
 const router = express.Router();
 
-router.get('/', function (req, res) {
+router.get('/',  (req, res) => {
   res.render('welcome');
 });
 
-router.get('/signup', function (req, res) {
+router.get('/signup',  (req, res) => {
   res.render('signup');
 });
 
-router.get('/login', function (req, res) {
+router.get('/login',  (req, res) => {
   res.render('login');
 });
 
-router.post('/signup', async function (req, res) {});
+router.post('/signup', async (req, res) => {
+  const userData = req.body;
+  const email = userData.email;
+  const password = userData.password;
+  const confirmEmail = userData['confirm-email'];
 
-router.post('/login', async function (req, res) {});
+  const hashedPassword = await bcrypt.hash(password, 12);
 
-router.get('/admin', function (req, res) {
+  const user = {
+    email: email,
+    password: hashedPassword,
+  }
+
+  await db.getDb().collection('users').insertOne(user);
+
+  res.redirect('/login');
+});
+
+router.post('/login', async (req, res) => {
+  const userData = req.body;
+  const email = userData.email;
+  const password = userData.password;
+
+  const user = await db.getDb().collection('users').findOne({ email: email });
+
+  if (!user) {
+    return res.redirect('/login');
+  }
+
+  const isValid = await bcrypt.compare(password, user.password);
+
+  if (isValid) {
+    req.session.isLoggedIn = true;
+    req.session.user = user;
+    return req.session.save(function (err) {
+      res.redirect('/admin');
+    });
+  }
+
+  res.redirect('/login');
+});
+
+router.get('/admin',  (req, res) => {
   res.render('admin');
 });
 
-router.post('/logout', function (req, res) {});
+router.post('/logout',  (req, res) => {});
 
 module.exports = router;
