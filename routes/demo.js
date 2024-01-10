151f27841487a6e8942d12e3d4c6225460a18dec
Heizenberg <popov.roman91@gmail.com>
2024-01-10 10:57:47 +0200

    added login and signup verification

--- routes/demo.js
+++ routes/demo.js
@@ -1,69 +1,82 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const bcrypt = require("bcryptjs");
 
-const db = require('../data/database');
-
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/',  (req, res) => {
-  res.render('welcome');
+router.get("/", (req, res) => {
+  res.render("welcome");
 });
 
-router.get('/signup',  (req, res) => {
-  res.render('signup');
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
-router.get('/login',  (req, res) => {
-  res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
   const userData = req.body;
   const email = userData.email;
   const password = userData.password;
-  const confirmEmail = userData['confirm-email'];
+  const confirmEmail = userData["confirm-email"];
+
+  if (
+    !email ||
+    !password ||
+    !confirmEmail ||
+    email !== confirmEmail ||
+    !email.includes("@")
+  ) {
+    console.log("Invalid input");
+    return res.redirect("/signup");
+  }
+
+  const existingUser = await db.getDb().collection('users').findOne({ email: email });
+
+  if (existingUser) {
+    console.log('User already exists');
+    return res.redirect('/signup');
+  }
 
   const hashedPassword = await bcrypt.hash(password, 12);
 
   const user = {
     email: email,
     password: hashedPassword,
-  }
+  };
 
-  await db.getDb().collection('users').insertOne(user);
+  await db.getDb().collection("users").insertOne(user);
 
-  res.redirect('/login');
+  res.redirect("/login");
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const userData = req.body;
   const email = userData.email;
   const password = userData.password;
 
-  const user = await db.getDb().collection('users').findOne({ email: email });
+  const user = await db.getDb().collection("users").findOne({ email: email });
 
   if (!user) {
-    return res.redirect('/login');
+    return res.redirect("/login");
   }
 
   const isValid = await bcrypt.compare(password, user.password);
 
-  if (isValid) {
-    req.session.isLoggedIn = true;
-    req.session.user = user;
-    return req.session.save(function (err) {
-      res.redirect('/admin');
-    });
+  if (!isValid) {
+    return res.redirect("/login");
   }
 
-  res.redirect('/login');
+  res.redirect("/admin");
 });
 
-router.get('/admin',  (req, res) => {
-  res.render('admin');
+router.get("/admin", (req, res) => {
+  res.render("admin");
 });
 
-router.post('/logout',  (req, res) => {});
+router.post("/logout", (req, res) => {});
 
 module.exports = router;
